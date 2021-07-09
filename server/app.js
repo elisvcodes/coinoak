5427357b720e0f4a6f374d3a485974766de03ab1
elisvcodes <sultanoveli@outlook.com>
2021-07-09 10:27:55 -0400

    updated app.js

--- server/app.js
+++ server/app.js
@@ -5,9 +5,13 @@ require('dotenv').config();
 require('./db/db');
 
 // Admin Middleware
-app.use('/api/v1/coin', require('./routes/coin'));
+app.use('/api/v1/admin/coin', require('./routes/admin/coin'));
+
+// Coins Middleware
+app.use('/api/v1/coins', require('./routes/coins/coin'));
 
 // Assets middleware
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 const PORT = process.env.PORT ? process.env.PORT : 7000;
 app.listen(PORT);
